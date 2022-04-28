066b121e2701389ae1120e8fc71b9c94b9965b01
h_gri <h_gri@HGLaptop5>
2022-04-28 17:14:47 +0200

    Part B, step 3 OK. Alt testet og OK i Postman

--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
@@ -19,7 +19,13 @@ public class AccessLog {
 	// TODO: add an access entry to the log for the provided message and return assigned id
 	public int add(String message) {
 		
-		int id = 0;
+		int id = cid.getAndIncrement(); //henter verdi, og øker
+		
+		//nytt element som tildeles nyeste id og meldingen
+		AccessEntry newEntry = new AccessEntry(id, message);
+		
+		//legger i loggen
+		log.put(id, newEntry); //id er her 
 		
 		return id;
 	}
@@ -27,20 +33,62 @@ public class AccessLog {
 	// TODO: retrieve a specific access entry from the log
 	public AccessEntry get(int id) {
 		
-		return null;
+		AccessEntry retrievedEntry = log.get(id);
+		
+		return retrievedEntry;
 		
 	}
 	
 	// TODO: clear the access entry log
 	public void clear() {
 		
+		log.clear();
+		cid.set(0); //siden vi tømmer køen må teller starte på 0 igjen :)
+		
 	}
 	
 	// TODO: return JSON representation of the access log
 	public String toJson () {
+		/*
+		 * 
+		 * {
+			   "id": 1,
+			   "message": "locked"
+			}
+		 */
+		
+		//MÅ SJEKKES OM FUNKER!
     	
-		String json = null;
-    	
+//		Gson gson = new Gson();
+//		
+//		String json = gson.toJson(this);
+//		
+//		int numberOfLogEntries = cid.get();
+		
+//		for (int i = 0; i < numberOfLogEntries; i++) {
+//			
+//			
+//		}
+		
+		//VURDERE EN ANNEN VARIANT!
+//		String json = null;
+//		json = "[";
+//		int value = cid.get();
+//		for (int i = 0; i < value; i++) {
+//			json +="{";
+//			json += "\"id\": "+i+",";
+//			json += "\"message\": \"" + log.get(i).getMessage() + "\"";
+//			json +="}";
+//			if (i != value - 1) {
+//				json +=",";
+//			}
+//		}
+//		json += "]";
+//    	
+		Gson gson = new Gson();
+		String json = gson.toJson(this);
     	return json;
+
+    	
     }
 }
--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
@@ -42,9 +42,73 @@ public class App {
 		 	
 		 	return gson.toJson("IoT Access Control Device");
 		});
-		
+				
 		// TODO: implement the routes required for the access control service
 		// as per the HTTP/REST operations describined in the project description
+		
+		//legger et element (innloggingsforsøk) til i loggen
+		post("/accessdevice/log", (req, res) -> {
+
+			Gson gson = new Gson();
+			AccessEntry entry = gson.fromJson(req.body(), AccessEntry.class); //bodyen til HTTP POST er en JSON reprensentasjon av AccesnEntry klassen obj
+			
+			//må se på denne? (printer tallet cid føst i loggen?)
+			int id = accesslog.add(entry.getMessage()); //.add returnerer IDen på meldingen som ble lagt til i loggen
+			res.body(gson.toJson(accesslog.get(id))); //
+			
+			return res;
+													
+		});
+		
+		//henter ut hele loggen
+		get("/accessdevice/log", (req, res) -> {
+			
+			//MÅ MULIGENS HA EN LØKKE HER ELLER I METODEN? Nei :)
+			
+			Gson gson = new Gson();
+			return gson.toJson(accesslog.toJson());		//toJson() metoden som definer i tidligere del. Usikker på om den funker?
+		});
+		
+		//henter ut et gitt element i loggen basert på ID
+		get("/accessdevice/log/:id", (req, res) -> {
+			
+			// får feil på mappingen i Spark ?!
+			
+			Gson gson = new Gson();
+			
+			return gson.toJson(accesslog.get(Integer.parseInt(req.params(":id"))));
+			
+			//LEGGE INN SJEKK OM LOCKED ELLER IKKE?
+		});
+		
+		//Sletter logen
+		
+		delete("/accessdevice/log", (req, res) -> {
+			accesslog.clear();
+			
+			//returnere tom logg
+			Gson gson = new Gson();
+			return gson.toJson(accesslog.toJson());	
+		});
+		
+		//Setter koden basert på verdi fra JSON fra requesten
+		put("/accessdevice/code", (req, res) -> {
+
+			Gson gson = new Gson();
+			accesscode = gson.fromJson(req.body(), AccessCode.class); //Henter fra bodyen til reqquesten og setter verdien som ny kode
+			
+			return gson.toJson(accesscode); //returnere samme JSON obj
+		});
+
+		//Henter kode
+		get("/accessdevice/code", (req, res) -> {
+
+			Gson gson = new Gson();
+
+			return gson.toJson(accesscode);
+		});
+		
+		
 		
     }
     
